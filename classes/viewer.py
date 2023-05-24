3891b6b0e4255f91645cf8d7a9fb441cdbbc18ac
Nathan <nathan.gracia.863@gmail.com>
2023-05-24 02:19:50 +0200

    maj

---
+++ classes/viewer.py
@@ -0,0 +1,8 @@
+class Viewer:
+    def __init__(self, name, timestamp):
+        self.name = name
+        self.updated_at = timestamp
+
+    def __repr__(self):
+        return self
+
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+import datetime
 from twitchio.ext import commands
 import sqlite3
 import mysql.connector
 from termcolor import colored
-
+from classes.viewer import Viewer
 
 # DB connection
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="",
-  database="deuskiwit"
+  database="toxicity_check"
 )
 mycursor = mydb.cursor()
 message_pack_length = 100
+# on rajoute un hours_delta pour correspondre au fuseau horraire, parceque je sais pas utiliser les datetime python
 
+hours_delta = 1
 channels_color = {"ponce" : 'magenta',
                   "otplol_" : 'red',
                   "chap_gg" : 'blue',
@@ -22,22 +25,39 @@ channels_color = {"ponce" : 'magenta',
                   'gotaga' : 'cyan'}
 
 messages = []
-vals = []
-def save(messages):
+messages_vals = []
+def save_message():
     # REQUETE SQL ###################################################################
-    sql = "INSERT INTO messages (content, user, channel, user_color, created_at) VALUES (%s, %s, %s, %s, %s)"
+    sql = "INSERT INTO message (content, viewer, channel, viewer_color, created_at) VALUES (%s, %s, %s, %s, %s)"
 
     for message in messages :
-        vals.append((message.content, message.author.name, message.channel.name, message.author.color, message.timestamp))
+        messages_vals.append((message.content, message.author.name, message.channel.name, message.author.color, message.timestamp+ datetime.timedelta(hours = hours_delta)))
 
 
-    mycursor.executemany(sql, vals)
+    mycursor.executemany(sql, messages_vals)
 
     mydb.commit()
 
     # Clear le tableau messages une fois envoyé
     messages.clear()
-    vals.clear()
+    messages_vals.clear()
+
+viewers = []
+viewers_vals = []
+def save_viewers():
+    sql = "REPLACE INTO viewer (name, updated_at) VALUES (%s, %s);"
+    for viewer in viewers :
+        viewers_vals.append( (viewer.name, viewer.updated_at))
+    mycursor.executemany(sql, viewers_vals)
+    mydb.commit()
+
+     # Clear le tableau viewers une fois envoyé
+    viewers.clear()
+    viewers_vals.clear()
+
+def save():
+    save_message()
+    save_viewers()
 
 class Bot(commands.Bot):
 
@@ -45,23 +65,41 @@ class Bot(commands.Bot):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
-        super().__init__(token='oauth:osmqsanyv3s1k8t0pvw3we6eslqjgi', prefix='?', initial_channels=['ponce', 'otplol_', 'chap_gg', 'sardoche', 'amouranth', 'gotaga'])
+        super().__init__(token='oauth:osmqsanyv3s1k8t0pvw3we6eslqjgi', prefix='?', initial_channels=['Sardoche'])
+        #super().__init__(token='oauth:osmqsanyv3s1k8t0pvw3we6eslqjgi', prefix='?', initial_channels=['ponce', 'otplol_', 'chap_gg', 'sardoche', 'amouranth', 'gotaga', 'DeusKiwi'])
 
     async def event_ready(self):
         # Notify us when everything is ready!
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
 
+    async def event_mode(self, Channel, User, status: str):
+        print("#################### MODERATION #################")
+        print("chaine : " + str(Channel))
+        print("User : " + str(User))
+        print("status : " + str(status))
+
+
+
+
     async def event_message(self, message) -> None:
+
+        viewer = Viewer(message.author.name, message.timestamp + datetime.timedelta(hours = hours_delta))
+        viewers.append(viewer)
+
         color = 'white'
+
         if message.channel.name in channels_color:
             color = channels_color[message.channel.name]
+
+
         print("[" + str(len(messages)) + "] " + colored(str(message.author.name), color) + " : " + str(message.content))
+
         messages.append(message)
 
         if(len(messages) >= message_pack_length ):
             print("#################### Ca fait 100 messages #################")
-            save(messages)
+            save()
 
 
     @commands.command()
